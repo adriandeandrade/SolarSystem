2330dda7f2ef2fedc6bcd6edc6dcf320a8c4c977
Adrian De Andrade <redfoxhint4@hotmail.com>
2018-11-07 01:48:22 -0500

    Built solar system

---
+++ Assets/Scripts/PlanetOrbiter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetOrbiter : MonoBehaviour
+{
+    [SerializeField] private Transform targetToRotateAround;
+    [SerializeField] private float orbitSpeed;
+
+    [SerializeField] private GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void Update()
+    {
+        transform.RotateAround(targetToRotateAround.position, Vector3.up, orbitSpeed * gameManager.orbitScaleSpeed * Time.deltaTime);
+    }
+}
